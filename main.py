0ed599741f256012b5675a3a970a6c129c20efe1
Louis Svugr <l.svugr@gmail.com>
2018-11-05 09:18:14 +0100

    coerce type of tg_name to str

--- main.py
+++ main.py
@@ -388,7 +388,7 @@ def ban(bot, userid, chatid):
 
     bot.restrict_chat_member(chatid, userid, until_date = (datetime.now() + timedelta(seconds=BAD_USER_BAN_TIME)).timestamp(), can_send_messages = False)
     logger.warning(f'{userid} id has been restricted from posting for 15 days')
-    bot.sendMessage(chatid, tg_name + texts.BANNED)
+    bot.sendMessage(chatid, str(tg_name) + texts.BANNED)
 
 def increment_good_counter(whom):
     conn = sqlite3.connect(DB_NAME)
