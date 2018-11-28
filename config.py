30a26b441849308a9fedaaea1a6e55f6e5318761
Louis Svugr <l.svugr@gmail.com>
2018-11-28 16:44:57 +0100

    remove string coercion of env vars

--- config.py
+++ config.py
@@ -2,18 +2,18 @@
 import os
 
 # CREDENTIALS
-TOKEN = os.environ.get('TG_TOKEN')  # restart the main script MANUALLY after this field is changed
-ADMIN_PASSWORD = os.environ.get('ADMIN_PW')  # to use special bot commands
+TOKEN = os.getenv(TG_TOKEN)  # restart the main script MANUALLY after this field is changed
+ADMIN_PASSWORD = os.getenv(ADMIN_PW)  # to use special bot commands
 
 # GROUPS
-CHAT_GROUP = os.environ.get('TG_CHAT_GROUP')
+CHAT_GROUP = os.getenv(TG_CHAT_GROUP)
 
 # INSTA_CREDENTIALS
-INSTA_USERNAME = os.environ.get('INSTA_USER')
-INSTA_PASSWORD = os.environ.get('INSTA_PW')
+INSTA_USERNAME = os.getenv(INSTA_USER)
+INSTA_PASSWORD = os.getenv(INSTA_PW)
 
 # DATABASE_URL
-DATABASE_URL = os.environ.get('DATABASE_URL')
+DATABASE_URL = os.getenv(DATABASE_URL)
 
 # TIMES
 # ROUNDS_INTERVAL = 23 * 60 * 60  # interval between rounds, seconds
