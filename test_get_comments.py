a5aacb8984dff4a917e863673baabb44a4800cd9
Louis Svugr <l.svugr@gmail.com>
2019-01-19 13:40:03 +0100

    remove async from check

--- test_get_comments.py
+++ test_get_comments.py
@@ -1,3 +1,88 @@
 # test get insta_comments
 
 from InstagramAPI import InstagramAPI
+import logging
+
+API = InstagramAPI('louis.sugar', 'fireflyer')
+API.login()
+
+# def get_pic_engagements(user):
+#     try:
+#         api.searchUsername(user)
+#         id = str(api.LastJson.get('user', "").get("pk", ""))
+#         api.getUserFeed(id)
+#         post_id = str(api.LastJson.get('items', "")[0].get("pk", ""))
+#         api.getMediaLikers(post_id)
+#         likers_handles = []
+#         for i in api.LastJson['users']:
+#             print(str(i.get('username', "")))
+#     except Exception as e:
+#         logging.exception(e)
+#         raise
+#
+# get_pic_engagements('cherrydeck')
+
+# def getComments(api, post_id):
+#     next_max_id = True
+#     while next_max_id:
+#         try:
+#             if next_max_id is True:
+#                 next_max_id = ''
+#             _ = api.getMediaComments(post_id, max_id=next_max_id)
+#             for i in api.LastJson['comments']:
+#                 try:
+#                     commentator = i.get('user', "").get('username', "")
+#                     print(commentator)
+#                 except Exception as e:
+#                     logger.warning('error while getting username from comment')
+#                     logger.exception(e)
+#                     raise
+#             next_max_id = api.LastJson.get('next_max_id', '')
+#         except Exception as e:
+#             logger.warning('error while getting comments')
+#             logger.exception(e)
+#             raise
+#
+# getComments(api, '1959726671447903328')
+
+# def getTotalFollowers(api, user_id):
+#     """
+#     Returns the list of followers of the user.
+#     It should be equivalent of calling api.getTotalFollowers from InstagramAPI
+#     """
+#
+#     followers = []
+#     next_max_id = True
+#     while next_max_id:
+#         # first iteration hack
+#         if next_max_id is True:
+#             next_max_id = ''
+#
+#         _ = api.getUserFollowers(user_id, maxid=next_max_id)
+#         followers.extend(api.LastJson.get('users', []))
+#         next_max_id = api.LastJson.get('next_max_id', '')
+#     print(followers)
+#
+# getTotalFollowers(api, '17950676368225416')
+
+# from InstagramAPI import InstagramAPI
+import time
+from datetime import datetime
+
+user_id = '17950676368225416'
+
+
+API.getUsernameInfo(user_id)
+API.LastJson
+following = []
+next_max_id = True
+while next_max_id:
+    # first iteration hack
+    if next_max_id is True:
+        next_max_id = ''
+    _ = API.getUserFollowings(user_id, maxid=next_max_id)
+    following.extend(API.LastJson.get('users', []))
+    next_max_id = API.LastJson.get('next_max_id', '')
+
+for user in following:
+    print(user)
